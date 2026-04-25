0472696595d281e64a1c4568cfbc2a98dfa40313
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-25 05:55:57 +0300

    Refactor layout.js to define RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
-<script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        {/* THE 3D ENGINE */}
+        <script 
+          type="module" 
+          src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
+        ></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
